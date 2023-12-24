7012c2c6326bdcdfa8b75568e90e6601f9c89e4b
Tharusha Mihiran <tharuxonline@gmail.com>
2023-12-25 02:53:40 +0530

    Search Page and Profile Page

--- Hippra/DesignConversion/dashboard/js/post.js
+++ Hippra/DesignConversion/dashboard/js/post.js
@@ -2,11 +2,13 @@
 
 const tabButtons = document.querySelectorAll(".post-tab-btn");
 const tabContents = document.querySelectorAll(".post-tab-content");
+const headingAction = document.querySelectorAll(".heading-actions");
 const mainLayout = document.querySelector(".main-layout");
 
 tabButtons.forEach((item) => {
   item.addEventListener("click", (e) => {
     const contentTabName = e.target.getAttribute("data-target");
+    const contentFilter = e.target.getAttribute("data-action");
 
     if (contentTabName === "post-ask-4-content") {
       mainLayout.classList.add("orange");
@@ -21,6 +23,12 @@ tabButtons.forEach((item) => {
     tabContents.forEach((item) => {
       item.classList.remove("active");
     });
+
+    headingAction.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    document.querySelector(`#${contentFilter}`).classList.add("active");
 
     e.target.classList.add("active");
     document.querySelector(`#${contentTabName}`).classList.add("active");
