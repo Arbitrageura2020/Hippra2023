7247b8855f225b5f5a76a63943db85e43483810a
Daniel Serafimovikj <danielserafimovik@gmail.com>
2024-04-01 15:39:53 +0200

    fixed serverside rendering a issues wand add comments

---
+++ Hippra/Models/Enums/Enums.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace Hippra.Models.Enums
+{
+    public static class Enums
+    {
+
+        // Helper method to display the name of the enum values.
+        public static string GetDisplayName(Enum value)
+        {
+            if (value.GetType().GetMember(value.ToString()).Count() > 0)
+            {
+                return value.GetType()?
+               .GetMember(value.ToString())?.First()?
+               .GetCustomAttribute<DisplayAttribute>()?
+               .Name;
+            }
+            else return "";
+        }
+
+        public static T GetValueByShortName<T>(this string shortName)
+        {
+            var values = from f in typeof(T).GetFields(BindingFlags.Static | BindingFlags.Public)
+                         let attribute = Attribute.GetCustomAttribute(f, typeof(DisplayAttribute)) as DisplayAttribute
+                         where attribute != null && attribute.Name == shortName
+                         select (T)f.GetValue(null);
+
+            if (values.Count() > 0)
+            {
+                return (T)(object)values.FirstOrDefault();
+            }
+
+            return default(T);
+        }
+    }
+}
